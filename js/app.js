ee62d8a0af4c4fa185aca0f81e6358bf29232306
Andy Fiedler <andyfiedler@Andys-MacBook-Pro.local>
2018-06-13 13:27:08 -0500

    add 'for loop'

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 //var storeList = document.getElementById
 
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', 
+'7pm', '8pm', '9pm']
 
 //Algonqiun Blvd, Sandy Shores
 var algonquinBlvd = {
@@ -12,10 +14,17 @@ var algonquinBlvd = {
     customerCount: function(){
         return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
     },
+    //15=store hours per day
     calculateGunsPerHour: function(){
         for (var i = 0; i<15; i++){
             this.gunsPerHour.push(Math.round(this.customerCount() * this.avgCustomers));
         }
+
+        for{var i=0; i<algonquinBlvd.gunsPerHour.length; i++}
+            var numGuns = algonquinBlvd.gunsPerHour[i];
+            var gunElement = document.createElement('li');
+            gunElement.textcontent = numGuns + 'guns';
+            elem.appendChild(gunElement);           
     }
 }
 algonquinBlvd.calculateGunsPerHour();
